39caa15ddda5cba581e19bbf9efcb4d9be2b93af
Samuel Serna Wills <samuel.sernaw@gmail.com>
2015-01-22 12:53:52 -0500

    Ejercicio suma de divisores 3 y 5 - Funcionando

--- divisores_3_5.rb
+++ divisores_3_5.rb
@@ -1,7 +1,7 @@
 div_cinco = []
 div_tres = []
 
-for i in (1..10)
+for i in (1..1000)
 	if i%3 == 0 and i%5 != 0
 		div_tres << i
 	elsif i%5 == 0
@@ -9,16 +9,26 @@ for i in (1..10)
 	end
 end
 
-sum_cinco = 0
-div_cinco.each  {|res_cinco|
-	
-	puts "#{res_cinco}"
+# From StackOverflow --> http://stackoverflow.com/questions/1538789/how-to-sum-array-of-numbers-in-ruby
+
+suma_tres = div_tres.inject (:+)
+suma_cinco = div_cinco.inject (:+)
+
+puts "#{suma_tres + suma_cinco}"
+
+
+# From viarails --> http://viarails.net/q/How-to-sum-an-array-of-numbers-in-Ruby
+
+suma_tres2 = 0
+
+div_tres.each do |i|
+	suma_tres2 += i
 end
 
+suma_cinco2 = 0
 
-puts "#{div_tres}"
-puts "#{div_cinco}"
-		
+div_cinco.each do |j|
+	suma_cinco2 += j
+end
 
-		
-
+puts "#{suma_tres2 + suma_cinco2}"
