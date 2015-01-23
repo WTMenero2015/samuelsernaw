7a65447b314ebc3298247c9145d1043ce444d3af
Samuel Serna Wills <samuel.sernaw@gmail.com>
2015-01-23 18:33:27 -0500

    Agrego método para realizar el cálculo

--- divisores_3_5.rb
+++ divisores_3_5.rb
@@ -1,13 +1,16 @@
+
+
 div_cinco = []
 div_tres = []
 
-for i in (1..1000)
+for i in (1..999)
 	if i%3 == 0 and i%5 != 0
 		div_tres << i
 	elsif i%5 == 0
 		div_cinco << i
 	end
 end
+
 
 # From StackOverflow --> http://stackoverflow.com/questions/1538789/how-to-sum-array-of-numbers-in-ruby
 
@@ -17,7 +20,7 @@ suma_cinco = div_cinco.inject (:+)
 puts "#{suma_tres + suma_cinco}"
 
 
-# From viarails --> http://viarails.net/q/How-to-sum-an-array-of-numbers-in-Ruby
+# From ViaRails --> http://viarails.net/q/How-to-sum-an-array-of-numbers-in-Ruby
 
 suma_tres2 = 0
 
@@ -32,3 +35,19 @@ div_cinco.each do |j|
 end
 
 puts "#{suma_tres2 + suma_cinco2}"
+
+# Mi manera
+
+suma3 = 0
+suma5 = 0
+
+for j in (1..999)
+	
+	if j%3 == 0 and j%5 != 0
+		suma3 = suma3 + j
+	elsif j%5 == 0
+		suma5 = suma5 + j
+	end
+end
+
+puts "#{suma3 + suma5}"
